154d0337c1ccceb1f5e7eeab49340ff7dadb4438
«Rinat» <rtatarin949@gmail.com>
2023-02-27 01:29:05 +0300

    day 1

--- Program.cs
+++ Program.cs
@@ -1,10 +1,33 @@
-﻿namespace CBfitness
+﻿using CBfitness.BL.Controller;
+using CBfitness.BL.Model;
+
+
+namespace CBfitness
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, in programm GBfitness");
+            
+            Console.WriteLine("Write name User");
+            var name = Console.ReadLine();
+
+            Console.WriteLine("Write gender ");
+            var gender = Console.ReadLine();
+
+            Console.WriteLine("Write date Brithday ");
+            var birthday = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write weigth ");
+            var weigth = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Write heigth ");
+            var heigth = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(name, gender, birthday, weigth, heigth);
+            userController.Save();
+
         }
     }
 }
